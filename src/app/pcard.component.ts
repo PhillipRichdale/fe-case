7dcc2c2c1994e89caad3478e9824d099fdf0599d
Phillip Richdale <office@richdale.de>
2024-01-16 18:58:09 +0100

    wip, various changes: pcard redone, post tryouts: moved pcard into own component, versioning old code for later reference in pcard-old (might kick that out), other small changes

---
+++ src/app/pcard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'pcard',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './pcard.component.html',
+  styleUrl: './pcard.component.css'
+})
+export class PcardComponent {}
